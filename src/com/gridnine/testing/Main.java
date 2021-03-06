8b87fa8e70277f63f70c906be924a81921407ab1
unknown <mikal99@mail.ru>
2021-03-07 01:19:07 +0300

    lambda is replaced with a method reference

--- src/com/gridnine/testing/Main.java
+++ src/com/gridnine/testing/Main.java
@@ -1,5 +1,6 @@
 package com.gridnine.testing;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,26 +10,25 @@ public class Main {
         // TODO: add debug class
         List<Flight> flights = FlightBuilder.createFlights();
 
-        // output start flights
-        System.out.println("Start:");
+        System.out.println("Start list:");
         printFlightList(flights);
 
         List<Flight> flights1 = flights.stream().
-                filter(x -> FlightValidator.departureUntilCurrentTime(x)).
+                filter(FlightValidator::departureAfterCurrentTime).
                 collect(Collectors.toList());
 
         System.out.println("\nAfter first rule:");
         printFlightList(flights1);
 
         List<Flight> flights2 = flights.stream().
-                filter(x -> FlightValidator.arrivalEarlierThanDeparture(x)).
+                filter(FlightValidator::arrivalEarlierThanDeparture).
                 collect(Collectors.toList());
 
         System.out.println("\nAfter second rule:");
         printFlightList(flights2);
 
         List<Flight> flights3 = flights.stream().
-                filter(x -> FlightValidator.moreThenTwoHoursOnGround(x)).
+                filter(FlightValidator::moreThenTwoHoursOnGround).
                 collect(Collectors.toList());
 
         System.out.println("\nAfter third rule:");
